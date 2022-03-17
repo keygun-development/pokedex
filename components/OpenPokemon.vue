69a3c3c6808082164931d0f0cb050c355549e8e8
Keygun2001 <keaganmulder1@gmail.com>
2022-03-17 11:29:54 +0100

    Added pokemon data + animations + colors

---
+++ components/OpenPokemon.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="c-overlay">
+        <div class="flex justify-center items-center h-full">
+            <div class="w-8/12 flex justify-center">
+                <div id="pokemon-single" class="c-pokemon__single-open" :class="type">
+                    <div class="flex justify-end">
+                        <button @click="close()" class="c-pokemon__single-close">
+                            X
+                        </button>
+                    </div>
+                    <div class="w-full flex">
+                        <div class="w-6/12 bg-white rounded-2xl">
+                            {{  }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+
+    props: {
+        pokemonName: String,
+        type: String,
+    },
+
+    watch: {
+        pokemonName: function(pokemonName){
+            this.pokemonName = pokemonName
+        },
+    },
+
+    methods: {
+        close () {
+            this.$emit('close')
+        }
+    }
+}
+</script>
--- components/Querybuilder.vue
+++ components/Querybuilder.vue
@@ -1,27 +1,58 @@
 <template>
     <div class="w-full flex flex-wrap">
-        <div class="w-1/5 c-pokemon__single mb-8 flex flex-col items-center" v-for="pokemon in pokemonEntries.slice(pokemonOffset, pokemonPerPage)">
-            <img :src="'https://projectpokemon.org/images/normal-sprite/'+pokemon.pokemon_species.name+'.gif'" />
-            {{ pokemon.pokemon_species.name }}
+        <div
+            class="w-1/5 my-8 flex"
+            v-for="(pokemon, index) in pokemonEntries.slice(pokemonOffset, pokemonPerPage)">
+            <div
+                @click="setOpenPokemon(pokemon.pokemon_species.name, type[index])"
+                class="c-pokemon__single flex flex-col items-center"
+                :class="type[index]">
+                <img
+                    :src="'https://projectpokemon.org/images/normal-sprite/'+pokemon.pokemon_species.name+'.gif'"
+                    @error="fallBackSprite($event, pokemon.pokemon_species.url)" />
+                {{ pokemon.pokemon_species.name }}
+            </div>
         </div>
         <div class="flex justify-between w-full">
             <a @click="previousPage()">Vorige</a>
+            <p>
+                {{ currentPage }}/{{ maxPage }}
+            </p>
             <a @click="nextPage()">Volgende</a>
         </div>
+        <caller></caller>
+        <open-pokemon
+        v-if="open"
+        :pokemon-name="clickedPokemon"
+        :type="clickedPokemonType"
+        @close="closePokemon()"
+        >
+
+        </open-pokemon>
     </div>
 </template>
 
 <script>
 
-export default {
+import OpenPokemon from "./OpenPokemon";
+import pokemonData from "../pokemonData"
+import caller from "./Caller"
 
+export default {
+    components: {OpenPokemon, caller},
     data() {
         return {
             baseUrl: 'https://pokeapi.co/api/v2/',
             pokemonResults: [],
             pokemonEntries: [],
             pokemonPerPage: 20,
-            pokemonOffset: 0
+            pokemonOffset: 0,
+            currentPage: 1,
+            maxPage: 0,
+            open: false,
+            clickedPokemon: '',
+            type: [],
+            clickedPokemonType: '',
         }
     },
 
@@ -45,7 +76,7 @@ export default {
 
     methods: {
         getPokemon: async function () {
-            await fetch(this.baseUrl+'pokedex/'+this.region+'?limit=20&offset=0')
+            await fetch(this.baseUrl+'pokedex/'+this.region)
                 .then(response => response.json())
                 .then(data => {
                     if (this.pokemonEntries.length > 0) {
@@ -57,12 +88,16 @@ export default {
                         this.pokemonEntries.push(data.pokemon_entries[i]);
                     }
                 })
+            this.maxPage = Math.ceil(this.pokemonEntries.length / 20);
+            this.getPokemonType();
         },
 
         previousPage: function () {
             if(this.pokemonOffset !== 0) {
                 this.pokemonOffset = this.pokemonOffset - 20;
                 this.pokemonPerPage = this.pokemonPerPage - 20;
+                this.currentPage -= 1;
+                this.getPokemonType();
             }
         },
 
@@ -70,10 +105,48 @@ export default {
             if((this.pokemonOffset + 20) < this.pokemonEntries.length) {
                 this.pokemonOffset = this.pokemonOffset + 20;
                 this.pokemonPerPage = this.pokemonPerPage + 20;
+                this.currentPage += 1;
+                this.getPokemonType();
             }
+        },
+
+        getPokemonType: function () {
+            if (this.type.length > 0) {
+                while (this.type.length) {
+                    this.type.pop();
+                }
+            }
+
+            for (let i = this.pokemonOffset; i<this.pokemonPerPage;i++) {
+                if(this.pokemonEntries[i].pokemon_species.name === pokemonData[i].name.english.toLowerCase()) {
+                    this.type.push(pokemonData[i].type[0])
+                } else {
+                    this.type.push("Normal")
+                }
+            }
+        },
+
+        fallBackSprite: function(e, pokemonUrl) {
+            e.target.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'+this.getIdByUrl(pokemonUrl)+'.png'
+        },
+
+        getIdByUrl: function (pokemonUrl) {
+            const arr = pokemonUrl.split('/')
+            return arr[arr.length - 2];
+        },
+
+        closePokemon: function () {
+            self = this;
+            document.getElementById('pokemon-single').classList.add('close')
+            setTimeout(function(){ self.open = false }, 3000);
+        },
+
+        setOpenPokemon: function (pokemonName, type) {
+            this.open = true;
+            this.clickedPokemon = pokemonName;
+            this.clickedPokemonType = type
         }
     }
-
 }
 
 </script>
