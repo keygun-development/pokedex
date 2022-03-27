7e13e8e0e79aed71cd9ab6c4b06ece6a6cec8e89
Keygun2001 <keaganmulder1@gmail.com>
2022-03-28 00:34:50 +0200

    Added basic information + Evolution chain + Colors + Next and prev btns

--- components/OpenPokemon.vue
+++ components/OpenPokemon.vue
@@ -1,17 +1,34 @@
 <template>
     <div class="c-overlay">
+        <div class="absolute flex items-center h-full">
+            <div @click="prev()" class="c-pokemon__next-prev --prev" v-if="singlePokemonSpecies.length > 0 && singlePokemonData[0].id-1 !== 0">
+                <img
+                    :src="'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'+(singlePokemonData[0].id-1)+'.png'"/>
+            </div>
+        </div>
+        <div class="absolute right-0 flex items-center h-full">
+            <div @click="next()" class="c-pokemon__next-prev --next" v-if="singlePokemonSpecies.length > 0 && singlePokemonData[0].id+1 !== 899">
+                <img
+                    :src="'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'+(singlePokemonData[0].id+1)+'.png'"/>
+            </div>
+        </div>
         <div class="flex justify-center items-center h-full">
             <div class="w-8/12 flex justify-center">
-                <div id="pokemon-single" class="c-pokemon__single-open" :class="type">
+                <div id="pokemon-single" class="c-pokemon__single-open" :class="changedType ? changedType : type">
                     <div class="flex justify-end">
                         <button @click="close()" class="c-pokemon__single-close">
                             X
                         </button>
                     </div>
                     <div class="w-full flex justify-between mt-4" v-if="singlePokemonSpecies.length > 0">
-                        <div class="flex flex-col items-center c-pokemon__single-image">
+                        <div class="flex flex-col items-center justify-between c-pokemon__single-image text-center">
                             <img
-                                :src="'https://projectpokemon.org/images/normal-sprite/'+singlePokemonData[0].name+'.gif'"/>
+                                :src="'https://projectpokemon.org/images/normal-sprite/'+singlePokemonData[0].name+'.gif'"
+                                @error="fallBackSprite($event, singlePokemonData[0].species.url)"
+                            />
+                            <p>
+                                {{ singlePokemonData[0].id }}. {{ singlePokemonData[0].name }}
+                            </p>
                         </div>
                         <div class="flex flex-col c-pokemon__single-stats ml-8">
                             <div class="flex flex-wrap justify-between w-full">
@@ -20,13 +37,39 @@
                                 </div>
                             </div>
                             <div class="mt-4">
-                                <p>
-                                    Base happiness: {{ singlePokemonSpecies[0].base_happiness }}
-                                </p>
-                                <p>
-                                    Capture rate: {{ singlePokemonSpecies[0].capture_rate }}
-                                </p>
+                                <div v-for="ability in singlePokemonData[0].abilities" class="flex">
+                                    <p>
+                                        {{ ability.ability.name }}
+                                    </p>
+                                    <p class="ml-2" v-if="ability.is_hidden">
+                                        (Hidden ability)
+                                    </p>
+                                </div>
                             </div>
+                            <div class="mt-4 flex flex-wrap">
+                                <div class="w-2/6" v-for="stat in singlePokemonData[0].stats">
+                                    <div class="c-pokemon__single-stat m-2" :class="stat.stat.name">
+                                        <p>
+                                            {{ stat.stat.name }} {{ stat.base_stat }}
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div v-if="evolutionData.length > 0" class="mt-4 w-full c-pokemon__single-evolution">
+                        <div class="flex justify-between">
+                            <img
+                                @click="getPokemonByEvolution(evolutionData[0].chain.species.url.split('/')[6])"
+                                :src="'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'+evolutionData[0].chain.species.url.split('/')[6]+'.png'"/>
+                            <img
+                                v-if="evolutionData[0].chain.evolves_to[0]"
+                                @click="getPokemonByEvolution(evolutionData[0].chain.evolves_to[0].species.url.split('/')[6])"
+                                :src="'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'+evolutionData[0].chain.evolves_to[0].species.url.split('/')[6]+'.png'"/>
+                            <img
+                                v-if="evolutionData[0].chain.evolves_to[0] && evolutionData[0].chain.evolves_to[0].evolves_to[0]"
+                                @click="getPokemonByEvolution(evolutionData[0].chain.evolves_to[0].evolves_to[0].species.url.split('/')[6])"
+                                :src="'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'+evolutionData[0].chain.evolves_to[0].evolves_to[0].species.url.split('/')[6]+'.png'"/>
                         </div>
                     </div>
                 </div>
@@ -42,7 +85,9 @@ export default {
     data() {
         return {
             singlePokemonData: [],
-            singlePokemonSpecies: []
+            singlePokemonSpecies: [],
+            evolutionData: [],
+            changedType: ""
         }
     },
 
@@ -82,8 +127,22 @@ export default {
             })
         },
 
+        getPokemonByEvolution: function (id) {
+            fetch("https://pokeapi.co/api/v2/pokemon/"+id)
+                .then(response => response.json())
+                .then(data => {
+                    if (this.singlePokemonData.length > 0) {
+                        while (this.singlePokemonData.length) {
+                            this.singlePokemonData.pop();
+                        }
+                    }
+                    this.singlePokemonData.push(data);
+                    this.getSinglePokemonSpecies(this.singlePokemonData[0].species.url)
+                })
+        },
+
         getSinglePokemonSpecies: async function (speciesUrl) {
-            fetch(speciesUrl)
+            await fetch(speciesUrl)
             .then(response => response.json())
             .then(data => {
                 if (this.singlePokemonSpecies.length > 0) {
@@ -93,6 +152,62 @@ export default {
                 }
                 this.singlePokemonSpecies.push(data);
             })
+            await this.getEvolutionChain(this.singlePokemonSpecies[0].evolution_chain.url)
+        },
+
+        getEvolutionChain: async function (evolutionChain) {
+            fetch(evolutionChain)
+                .then(response => response.json())
+                .then(data => {
+                    if (this.evolutionData.length > 0) {
+                        while (this.evolutionData.length) {
+                            this.evolutionData.pop();
+                        }
+                    }
+                    this.evolutionData.push(data);
+                })
+        },
+
+        prev: function () {
+            fetch("https://pokeapi.co/api/v2/pokemon/"+(this.singlePokemonData[0].id-1))
+                .then(response => response.json())
+                .then(data => {
+                    if (this.singlePokemonData.length > 0) {
+                        while (this.singlePokemonData.length) {
+                            this.singlePokemonData.pop();
+                        }
+                    }
+                    this.singlePokemonData.push(data);
+                    this.getSinglePokemonSpecies(this.singlePokemonData[0].species.url)
+                    this.setBackgroundByType();
+                })
+        },
+
+        fallBackSprite: function(e, pokemonUrl) {
+            e.target.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'+pokemonUrl.split('/')[6]+'.png'
+        },
+
+        next: function () {
+            fetch("https://pokeapi.co/api/v2/pokemon/"+(this.singlePokemonData[0].id+1))
+                .then(response => response.json())
+                .then(data => {
+                    if (this.singlePokemonData.length > 0) {
+                        while (this.singlePokemonData.length) {
+                            this.singlePokemonData.pop();
+                        }
+                    }
+                    this.singlePokemonData.push(data);
+                    this.getSinglePokemonSpecies(this.singlePokemonData[0].species.url)
+                    this.setBackgroundByType();
+                })
+        },
+
+        setBackgroundByType: function () {
+            if (this.singlePokemonData[0].types.length > 1 && this.singlePokemonData[0].types[0] === 'Normal') {
+                this.changedType = this.singlePokemonData[0].types[1].type.name
+            } else {
+                this.changedType = this.singlePokemonData[0].types[0].type.name
+            }
         }
     }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,6 @@
                 <Querybuilder
                 :region="selectedRegion"
                 >
-
                 </Querybuilder>
             </div>
         </div>
